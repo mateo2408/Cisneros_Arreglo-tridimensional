e1f68b4880296759e16affb6e502ab624810c8dc
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-30 18:50:01 -0500

    Input de usuario

--- Arrgelo_Tridimensional.c
+++ Arrgelo_Tridimensional.c
@@ -5,3 +5,21 @@ usuario. El programa debe inicializar todas las matrices del arreglo en ceros,
 a excepción de la última matriz que debe ser inicializada 
 en unos.*/
 
+#include<stdio.h>
+
+int main()
+{
+    //Se pide el tamaño de las matrices
+    int capas;
+    int filas;
+    int columnas;
+    printf("Ingrese el número de matrices: ");
+    scanf("%d", &capas);
+    printf("Ingrese el número de filas de las matrices: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de las matrices: ");
+    scanf("%d", &columnas);
+
+    
+    return 0;
+}
