15bc5ee453befef7bd6faaabc6a75cff4dafd2d8
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-30 18:49:10 -0500

    Creacion de proyecto

---
+++ Arrgelo_Tridimensional.c
@@ -0,0 +1,7 @@
+//Mateo Cisneros
+//30/11/2023
+/*Crear e imprimir todos los elementos de un arreglo tridimensional, cuyas dimensiones sean ingresadas por el 
+usuario. El programa debe inicializar todas las matrices del arreglo en ceros, 
+a excepción de la última matriz que debe ser inicializada 
+en unos.*/
+
